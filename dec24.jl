9e4871031f30793286ecd971763d73bedbfbe08e
Sajid Zaidi <sajidsrzaidi@gmail.com>
2023-12-24 01:31:50 -0500

    day 24

---
+++ dec24.jl
@@ -0,0 +1,76 @@
+### A Pluto.jl notebook ###
+# v0.19.32
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 3f480922-a21f-11ee-3d96-c9670550ea73
+using DelimitedFiles
+
+# ╔═╡ bb999aab-b398-42ae-b488-bbb16daa44d5
+readdlm("day24.txt",'@')
+
+# ╔═╡ a0bece0b-8dca-4953-9ace-366a051e1493
+m2=v2[2]/v2[1]
+
+# ╔═╡ ecc5c559-0ca6-436b-9115-de1c6239dab3
+p2[1]
+
+# ╔═╡ ce6f8403-d4bd-48ee-b422-f75309cf4b59
+
+
+# ╔═╡ 25d74aa7-0a22-4810-8d3d-512b8a59d381
+
+
+# ╔═╡ 7f672964-b764-40cd-bc96-2b59dcf95513
+
+
+# ╔═╡ b2f4346b-a187-44f4-b211-be79097a2978
+
+
+# ╔═╡ d4aba746-3268-4a53-809d-59b12f052d03
+typemax(Int64)-1
+
+# ╔═╡ 5eb19f6b-0973-4506-b664-9f6b75bb8f2a
+1/2==8/16
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+DelimitedFiles = "8bb1440f-4735-579b-a4ab-409b98df4dab"
+
+[compat]
+DelimitedFiles = "~1.9.1"
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.9.4"
+manifest_format = "2.0"
+project_hash = "eb696dba116621a072d91c57084a363ced7654e8"
+
+[[deps.DelimitedFiles]]
+deps = ["Mmap"]
+git-tree-sha1 = "9e2f36d3c96a820c678f2f1f1782582fcf685bae"
+uuid = "8bb1440f-4735-579b-a4ab-409b98df4dab"
+version = "1.9.1"
+
+[[deps.Mmap]]
+uuid = "a63ad114-7e13-5084-954f-fe012c677804"
+"""
+
+# ╔═╡ Cell order:
+# ╠═3f480922-a21f-11ee-3d96-c9670550ea73
+# ╠═bb999aab-b398-42ae-b488-bbb16daa44d5
+# ╠═a0bece0b-8dca-4953-9ace-366a051e1493
+# ╠═ecc5c559-0ca6-436b-9115-de1c6239dab3
+# ╠═ce6f8403-d4bd-48ee-b422-f75309cf4b59
+# ╠═25d74aa7-0a22-4810-8d3d-512b8a59d381
+# ╠═7f672964-b764-40cd-bc96-2b59dcf95513
+# ╠═b2f4346b-a187-44f4-b211-be79097a2978
+# ╠═d4aba746-3268-4a53-809d-59b12f052d03
+# ╠═5eb19f6b-0973-4506-b664-9f6b75bb8f2a
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
